e721248728c8561e0fc094e01ad03ebad1993810
Alessandro Lepore <lepore.ale@gmail.com>
2016-01-27 10:41:32 +0100

    user decorator changes

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,17 +1,27 @@
 Spree.user_class.class_eval do
   after_save :newsletter_subscription
 
+  def subscribe_to_newsletter!
+    update_attributes(subscribed_to_mailing_list: true)
+  end
+
   private
 
   def newsletter_subscription
     if id_changed? || subscribed_to_mailing_list_changed?
       if subscribed_to_mailing_list
-        params = { email: email,
-                   terms: '1' }
-        Spree::NewsletterSubscription.new(params).save!
+        Spree::NewsletterSubscription.new(newsletter_params).save!
       else
         # TODO unsubscribe?
       end
     end
+  end
+
+  # override to send more data to the provider
+  def newsletter_params
+    {
+      email: email,
+      terms: '1'
+    }
   end
 end
